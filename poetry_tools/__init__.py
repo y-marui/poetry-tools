2d0a9e29e2f24234ab8711090bdcc31831e86a71
Yukihiro <12211343+y-marui@users.noreply.github.com>
2022-04-14 17:57:15 +0900

    Fix bugs.

--- poetry_tools/__init__.py
+++ poetry_tools/__init__.py
@@ -1,1 +1,2 @@
-__version__ = '1.0.0'
+"""Poetry tools."""
+__version__ = '1.1.0'
--- poetry_tools/check_git_flow.py
+++ poetry_tools/check_git_flow.py
@@ -9,10 +9,32 @@ from pre_commit_hooks.util import cmd_output
 def _check_git_flow() -> int:
     """Check compatibility with git-flow.
 
-    If branch is `main` or `master`, run `pytest`.
+    Check version from package and poetry match.
+    Retruns exit code instead of raise Errors.
+
+    If branch is `main` or `master`,
+    * run `pytest`
+
     If branch is `hotfix/.*` or `release/.*`,
-    check version from branck and poetry match.
+    * check version from branch and poetry match
+    * check version from changelog and poetry match
     """
+    ref_name = cmd_output('poetry', 'version')
+    reponame, version_poetry = ref_name.strip().split()
+    reponame = reponame.replace("-", "_")
+    version_package = cmd_output(
+        "python",
+        "-c",
+        f"import {reponame}; print({reponame}.__version__)"
+    ).strip()
+    if version_package != version_poetry:
+        print(
+            f"Package returns `{version_package}` "
+            f"but Poetry returns `{version_poetry}`"
+        )
+        return 1
+
+    # Special Cases
     if is_on_branch(["main", "master"], []):
         res = subprocess.run(["poetry", "run", "pytest"])
         return res.returncode
@@ -22,12 +44,23 @@ def _check_git_flow() -> int:
         chunks = ref_name.strip().split('/')
         version_git_flow = '/'.join(chunks[3:])
 
-        version_poetry = cmd_output('poetry', 'version', "-s").strip()
-
         if version_git_flow != version_poetry:
             print(
                 f"Git flow work on `{version_git_flow}` "
-                f"but Poetry returns `{version_poetry}`")
+                f"but Poetry returns `{version_poetry}`"
+            )
+            return 1
+
+        with open("CHANGELOG.md") as fp:
+            fp.readline()
+            ref_name = fp.readline()
+            _, versiom_md, *_ = ref_name.strip().split()
+
+        if versiom_md != version_poetry:
+            print(
+                f"CHANGELOG.md returns `{versiom_md}` "
+                f"but Poetry returns `{version_poetry}`"
+            )
             return 1
 
     return 0
@@ -35,6 +68,17 @@ def _check_git_flow() -> int:
 
 @click.command()
 def check_git_flow():
+    """Check compatibility with git-flow.
+
+    Check version from package and poetry match.
+
+    If branch is `main` or `master`,
+    * run `pytest`
+
+    If branch is `hotfix/.*` or `release/.*`,
+    * check version from branch and poetry match
+    * check version from changelog and poetry match
+    """
     raise SystemExit(_check_git_flow())
 
 
