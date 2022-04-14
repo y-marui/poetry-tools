e2257fcd5cac9bd338561f4cd2f0e7fd916a585b
Yukihiro <12211343+y-marui@users.noreply.github.com>
2022-04-14 16:11:39 +0900

    Add log message.

--- poetry_tools/check_git_flow.py
+++ poetry_tools/check_git_flow.py
@@ -1,27 +1,26 @@
-# import argparse
-# import imp
-# import re
-# from typing import AbstractSet
-# from typing import Sequence
+"""check_git_flow: Check compatibility with git-flow."""
 import subprocess
 
-from pre_commit_hooks.util import cmd_output
-
-from pre_commit_hooks.no_commit_to_branch import is_on_branch
-
 import click
+from pre_commit_hooks.no_commit_to_branch import is_on_branch
+from pre_commit_hooks.util import cmd_output
 
 
 @click.command()
 def check_git_flow():
     """Check compatibility with git-flow.
-    
+
     If branch is `main` or `master`, run `pytest`.
-    If branch is `hotfix/.*` or `release/.*`, check version from branck and poetry match.
+    If branch is `hotfix/.*` or `release/.*`,
+    check version from branck and poetry match.
     """
+    print("Start Check")
+    ref_name = cmd_output('git', 'symbolic-ref', 'HEAD').strip()
+    print(f"You are on {ref_name}.")
+
     if is_on_branch(["main", "master"], []):
         res = subprocess.run(["poetry", "run", "pytest"])
-        return res.returncode 
+        return res.returncode
 
     elif is_on_branch([], ["hotfix/.*", "release/.*"]):
         ref_name = cmd_output('git', 'symbolic-ref', 'HEAD')
@@ -31,10 +30,13 @@ def check_git_flow():
         version_poetry = cmd_output('poetry', 'version', "-s").strip()
 
         if version_git_flow != version_poetry:
-            print(f"Git flow work on `{version_git_flow}` but Poetry returns `{version_poetry}`")
+            print(
+                f"Git flow work on `{version_git_flow}` "
+                "but Poetry returns `{version_poetry}`")
             return 1
 
     return 0
+
 
 if __name__ == '__main__':
     raise SystemExit(check_git_flow())
